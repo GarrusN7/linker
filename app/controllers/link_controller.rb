af7d425462bd21ae66ce1f794ecb0f9123b509ae
GarrusN7 <rsbanjoman@gmail.com>
2015-03-18 11:02:40 -0600

    Inital repository

---
+++ app/controllers/link_controller.rb
@@ -0,0 +1,22 @@
+class LinkController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
